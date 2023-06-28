f7ea50e221f9b07fd7c47fc18e30fd0413fb2ca8
William Castro <williamweb2.0@gmail.com>
2023-06-28 09:36:41 -0300

    Create SimpleBotRecon.sh

---
+++ SimpleBotRecon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SimpleBotRecon.sh
+#Author: William Castro
+#tools: subfinder,sublist3r,findomain,httpx,katana,amass,nuclei,fff,unfurl,gospider,dirsearch,dnsx,mapcidr,naabu
+
+#please wait, is coming here...
